ea9d332fb80994ccaa9eb7f96aa34d6515981e74
Simon Loffler <simon.loffler@acmi.net.au>
2019-05-07 14:13:41 +1000

    Start an X server.

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+rm /tmp/.X0-lock &>/dev/null || true
+
+export DISPLAY=:0
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+echo "Starting X in 2 seconds"
+sleep 2
+startx &
+
 # Start Flask
 python -u app/main.py &
 
