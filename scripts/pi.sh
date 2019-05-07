8b245f085c36d60a4f14557f0f2b643e7b78703a
Simon Loffler <simon.loffler@acmi.net.au>
2019-05-08 09:24:44 +1000

    Wait for X to start for the slower A+

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -7,6 +7,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Starting X in 2 seconds"
 sleep 2
 startx &
+sleep 10
 
 # Hide the cursor
 unclutter -display :0 -idle 0.1 &
