2139b64a9038e82da2291fed890efcc9045824cf
Simon Loffler <simon.loffler@acmi.net.au>
2019-05-15 09:56:33 +1000

    Move dbus environment variables from the bash script to the Dockerfile

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -2,8 +2,6 @@
 
 rm /tmp/.X0-lock &>/dev/null || true
 
-export DISPLAY=:0
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Starting X in 5 seconds"
 sleep 5
 startx &
