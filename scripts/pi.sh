286bbff80bdc67d0859297e9cb2ee55a3793e3b7
Simon Loffler <simon.loffler@acmi.net.au>
2019-05-15 09:42:24 +1000

    Try a longer pause for x to start

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -4,8 +4,8 @@ rm /tmp/.X0-lock &>/dev/null || true
 
 export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-echo "Starting X in 2 seconds"
-sleep 2
+echo "Starting X in 5 seconds"
+sleep 5
 startx &
 sleep 10
 
