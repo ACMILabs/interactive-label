0c30f9022b58b409d3b5a1a579f0b81ab1bd5f57
Simon Loffler <simon.loffler@acmi.net.au>
2019-07-31 12:03:11 +1000

    Save selected label to a local database so they can be collected with a lens tap.

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from threading import Thread
 
 import requests
 from flask import Flask, jsonify, render_template, request
+from peewee import CharField, IntegerField, Model, SqliteDatabase
+from playhouse.shortcuts import model_to_dict
 
 from errors import HTTPError
 
@@ -15,6 +17,17 @@ XOS_PLAYLIST_ID = os.getenv('XOS_PLAYLIST_ID')
 
 app = Flask(__name__)
 cached_playlist_json = f'playlist_{XOS_PLAYLIST_ID}.json'
+
+# instantiate the peewee database
+db = SqliteDatabase('label.db')
+
+
+class Label(Model):
+    datetime = CharField(primary_key=True)
+    label_id = IntegerField()
+    playlist_id = IntegerField()
+    class Meta:
+        database = db
 
 
 def download_playlist():
@@ -64,6 +77,28 @@ def playlist_json():
     return jsonify(json_data)
 
 
+@app.route('/label', methods=['POST'])
+def select_label():
+    """
+    Save the Label ID that was selected to a local database with the Date, and Playlist ID.
+    """
+    label_selected = dict(request.get_json())
+
+    # Save the label selected to the database
+    label = Label.create(
+        datetime = label_selected['datetime'],
+        playlist_id = label_selected.get('playlist_id', 0),
+        label_id = label_selected.get('label_id', 0),
+    )
+    # Clear out other messages beyond the last 5
+    delete_records = Label.delete().where(
+        Label.datetime.not_in(Label.select(Label.datetime).order_by(Label.datetime.desc()).limit(5))
+    )
+    delete_records.execute()
+
+    return jsonify(model_to_dict(label))
+
+
 @app.route('/tap', methods=['POST'])
 def collect_item():
     """
@@ -71,8 +106,8 @@ def collect_item():
     """
     xos_tap_endpoint = f'{XOS_API_ENDPOINT}taps/'
     xos_tap = dict(request.get_json())
-    record = {"playlist_id": 1}
-    xos_tap['label'] = record.pop('playlist_id', None)
+    record = model_to_dict(Label.select().order_by(Label.datetime.desc()).get())
+    xos_tap['label'] = record.pop('label_id', None)
     xos_tap.setdefault('data', {})['playlist_info'] = record
     headers = {'Authorization': 'Token ' + AUTH_TOKEN}
     response = requests.post(xos_tap_endpoint, json=xos_tap, headers=headers)
@@ -81,5 +116,6 @@ def collect_item():
     return jsonify(xos_tap), response.status_code
 
 if __name__ == '__main__':
+    db.create_tables([Label])
     download_playlist()
     app.run(host='0.0.0.0', port=8080)
