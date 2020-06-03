e510093dcaf11847e9b2b5154d9892a4a9121e17
Simon Loffler <simon.loffler@acmi.net.au>
2020-06-03 15:31:14 +0930

    Update the Raspberry Pi 4 startup script with learnings from the Playlist Label

--- scripts/pi4.sh
+++ scripts/pi4.sh
@@ -9,10 +9,17 @@ export DISPLAY=:0
 # Set the DBUS address for sending around system messages
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# Set XDG_RUNTIME_DIR
+mkdir -pv ~/.cache/xdgr
+export XDG_RUNTIME_DIR=$PATH:~/.cache/xdgr
+
+# Create Xauthority
+touch /root/.Xauthority
+
 # start desktop manager
 echo "STARTING X"
 sleep 2
-startx -- -nocursor &
+startx &
 sleep 20
 
 # Hide the cursor
@@ -26,18 +33,27 @@ sleep 10
 xset s off -dpms
 
 # Launch chromium browser in fullscreen on that page
-SCREEN_SCALE="${SCREEN_SCALE:-default 1.0}"
-LABEL_INTERACTIVE_PORT="${LABEL_INTERACTIVE_PORT:-default 8081}"
+SCREEN_SCALE="${SCREEN_SCALE:-1.0}"
+LABEL_INTERACTIVE_PORT="${LABEL_INTERACTIVE_PORT:-8081}"
 chromium-browser \
   --app=http://localhost:$LABEL_INTERACTIVE_PORT \
   --start-fullscreen \
   --no-sandbox \
   --user-data-dir \
   --kiosk \
-  --force-device-scale-factor=$SCREEN_SCALE
+  --disable-dev-shm-usage \
+  --disable-backing-store-limit \
+  --force-device-scale-factor=$SCREEN_SCALE \
+  --check-for-update-interval=31449600 \
+  --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'
 
 
 # For debugging
 echo "Chromium browser exited unexpectedly."
 free -h
 echo "End of pi.sh ..."
+
+# Restart the container manually
+echo "Restarting container..."
+curl -H "Content-Type: application/json" -d "{\"serviceName\": \"$BALENA_SERVICE_NAME\"}" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY"
+echo "End of pi.sh"
