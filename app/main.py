2ffcb60ce7d5048c8e859819aa9c7ed7dd73b06f
Sam Maher <sam.maher@acmi.net.au>
2021-07-05 13:19:09 +1000

    #100 Updated wording

--- app/main.py
+++ app/main.py
@@ -194,12 +194,12 @@ def event_stream():
         time.sleep(0.1)
         has_tapped = HasTapped.get_or_none(tap_processing=1, has_tapped=1)
         if has_tapped:
-            ui_tap_event = f'data: {{ "tap_successful": {has_tapped.tap_successful} }}\n\n'
+            tap_event_message = f'data: {{ "tap_successful": {has_tapped.tap_successful} }}\n\n'
             has_tapped.has_tapped = 0
             has_tapped.tap_processing = 0
             has_tapped.tap_successful = 0
             has_tapped.save()
-            yield ui_tap_event
+            yield tap_event_message
 
 
 @app.route('/api/tap-source/')
