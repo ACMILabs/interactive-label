bc67c7636293d98224a4479f22e0989c9b511594
Simon Loffler <simon.loffler@acmi.net.au>
2022-06-17 13:28:58 +0930

    #124 Add a notification bar to interactive labels to prompt visitors to tap images before tapping their lens

--- app/static/app.js
+++ app/static/app.js
@@ -62,6 +62,9 @@ let is_animating_collect = false;
 const collect_elements = [];
 let current_modal = null;
 const COLLECT_TEXT = "TO COLLECT TAP LENS ON READER";
+const NOTIFICATION_TEXT = "Tap an image to open label and collect";
+const notification_bar_timeout = 30000;
+const modal_timeout = 60000;
 
 const active_images = [];
 for (let i = 0; i < labels.length; i++) {
@@ -115,15 +118,52 @@ function close_modal() {
   save_label(null);
 }
 
+const notification_bar = document.createElement("div");
+notification_bar.className = "notification_bar";
+notification_bar.innerHTML = `<p>${NOTIFICATION_TEXT}</p>`;
+root.appendChild(notification_bar);
+
+function open_notification_bar() {
+  notification_bar.style.top = "0px";
+}
+
+function close_notification_bar() {
+  notification_bar.style.top = `-${notification_bar.clientHeight}px`;
+}
+
+notification_bar.addEventListener("click", close_notification_bar);
+
 let close_timer = null;
 function handle_timer_timeout() {
   if (current_modal) {
     close_modal();
   }
 }
+let close_notification_bar_timer = null;
+function handle_notification_bar_timer_timeout() {
+  if (!current_modal) {
+    open_notification_bar();
+  } else {
+    window.clearTimeout(close_notification_bar_timer);
+    close_notification_bar_timer = window.setTimeout(
+      handle_notification_bar_timer_timeout,
+      notification_bar_timeout
+    );
+  }
+}
+close_notification_bar_timer = window.setTimeout(
+  handle_notification_bar_timer_timeout,
+  notification_bar_timeout
+);
 window.addEventListener("click", function () {
   window.clearTimeout(close_timer);
-  close_timer = window.setTimeout(handle_timer_timeout, 60000);
+  close_timer = window.setTimeout(handle_timer_timeout, modal_timeout);
+  window.clearTimeout(close_notification_bar_timer);
+  close_notification_bar();
+  close_notification_bar_timer = window.setTimeout(
+    handle_notification_bar_timer_timeout,
+    notification_bar_timeout
+  );
 });
 
 function setModalTimeout(milliseconds) {
@@ -307,7 +347,7 @@ for (let i = 0; i < labels.length; i++) {
 }
 
 const paths_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-paths_svg.className = "paths_svg";
+paths_svg.classList.add("paths_svg");
 paths_svg.setAttribute(
   "viewBox",
   `0 0 ${window.data.background_dimensions[0]} ${window.data.background_dimensions[1]}`
@@ -413,13 +453,19 @@ function open_modal(errorText) {
 const tap_source = new EventSource("/api/tap-source");
 
 tap_source.onmessage = function (event) {
+  close_notification_bar();
+  window.clearTimeout(close_notification_bar_timer);
+  close_notification_bar_timer = window.setTimeout(
+    handle_notification_bar_timer_timeout,
+    notification_bar_timeout
+  );
   const event_data = JSON.parse(event.data);
   const tap_successful =
     event_data.tap_successful && event_data.tap_successful === 1;
 
   if (!active_collect_element && tap_successful) {
     open_modal(
-      "<h1>Tap an image to open label and collect.</h1><p>The first item on this interactive label has been added to your Lens collection.</p>"
+      `<h1>${NOTIFICATION_TEXT}</h1><p>The first item on this interactive label has been added to your Lens collection.</p>`
     );
     return;
   }
