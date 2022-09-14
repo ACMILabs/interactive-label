cdd9247d6fa138960811d1585587fce2d1923021
Sam Maher <sam.maher@acmi.net.au>
2022-09-14 10:18:33 +1000

    #130 Syntax updates

--- app/static/debug.js
+++ app/static/debug.js
@@ -1,5 +1,4 @@
-document.addEventListener('touchstart', debugTouchscreenEvent);
-document.addEventListener('click', debugTouchscreenEvent);
+const root = document.getElementById("root");
 
 const screenTouchedIcon = document.createElement("div");
 root.appendChild(screenTouchedIcon);
@@ -7,9 +6,12 @@ screenTouchedIcon.className = "screen_touched_icon";
 
 /** Show where the user last clicked */
 function debugTouchscreenEvent(event) {
-  mouseX = event.clientX - 15; // Minus half the width of the icon
-  mouseY = event.clientY - 15; // Minus half the height of the icon
+  const mouseX = event.clientX - 15; // Minus half the width of the icon
+  const mouseY = event.clientY - 15; // Minus half the height of the icon
 
   screenTouchedIcon.style.left = `${mouseX}px`;
   screenTouchedIcon.style.top = `${mouseY}px`;
 }
+
+document.addEventListener("touchstart", debugTouchscreenEvent);
+document.addEventListener("click", debugTouchscreenEvent);
